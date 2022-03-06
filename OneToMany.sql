cd0d4c221adcd9bb5984b159c946cb788ba8c0fc
Mohibur Rahman <mohiburrahman5509@gmail.com>
2022-03-06 16:36:26 +0600

    SQL file added.

---
+++ OneToMany.sql
@@ -0,0 +1,13 @@
+# onetomany database
+create database onetomany;
+
+# course table
+select * from course;
+drop table course;
+
+# drop a column
+alter table course drop column course_name;
+
+# department table
+select * from department;
+drop table department;
